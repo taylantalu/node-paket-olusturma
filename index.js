aa2ec21dea958be9940fe4412a5a89c6b170c213
taylantalu <taylantalu@gmail.com>
2019-08-28 09:51:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Merhaba uygulamama hoşgeldiniz");
+console.log("npm install paketadı yazıp yüklendiğinde, dependencies oluşturulup içine yüklü olan paketler ve versiyonları gelir.");
+console.log("package-lock.json ise, node_modules değiştiğinde bu değişimleri gösterir.örneğin paket versiyonu vb");
