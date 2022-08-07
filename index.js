4ea7ee6ed4738af7904a646736976163c406dd56
taylantalu <44743681+taylantalu@users.noreply.github.com>
2022-08-07 09:35:11 +0300

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("Merhaba uygulamama hoşgeldiniz");
-console.log("npm install paketadı yazıp yüklendiğinde, dependencies oluşturulup içine yüklü olan paketler ve versiyonları gelir.");
-console.log("package-lock.json ise, node_modules değiştiğinde bu değişimleri gösterir.örneğin paket versiyonu vb");
